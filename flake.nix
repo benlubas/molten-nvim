dbfebe20688375484f9bf8abc6b10903f4bd7e5b
benlubas <benmlubas@gmail.com>
2023-11-06 12:25:34 -0500

    update nix flake

--- flake.nix
+++ flake.nix
@@ -1,3 +1,5 @@
+# I'm very new to nix, so this is probably really ugly. If you want to contribute to improve this,
+# please do
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -30,19 +32,6 @@
 
             nodePackages.pyright
           ];
-          # Workaround in linux: python downloads ELF's that can't find glibc
-          # You would see errors like: error while loading shared libraries: name.so: cannot open shared object file: No such file or directory
-          # LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-          #   pkgs.stdenv.cc.cc
-          #   #pkgs.lib
-          #   # Add any missing library needed
-          #   # You can use the nix-index package to locate them, e.g. nix-locate -w --top-level --at-root /lib/libudev.so.1
-          # ];
-          # Put the venv on the repo, so direnv can access it
-          # POETRY_VIRTUALENVS_IN_PROJECT = "true";
-          # POETRY_VIRTUALENVS_PATH = "{project-dir}/.venv";
-          # Use python from path, so you can use a different version to the one bundled with poetry
-          # POETRY_VIRTUALENVS_PREFER_ACTIVE_PYTHON = "true";
         };
       }
     );
