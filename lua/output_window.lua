84aa2d34ee98e32da48992471101aaaf27b9a4dd
benlubas <benmlubas@gmail.com>
2023-12-13 19:32:02 -0500

    fix: account for windows not at the top of screen

--- lua/output_window.lua
+++ lua/output_window.lua
@@ -7,8 +7,10 @@ local M = {}
 M.calculate_window_position = function(buf_line)
   local win = vim.api.nvim_get_current_win()
   local pos = vim.fn.screenpos(win, buf_line, 0)
+  local topline = vim.fn.line('w0')
+  local win_origin = vim.fn.screenpos(win, topline, 0)
 
-  return pos.row
+  return pos.row - (win_origin.row - 1)
 end
 
 return M
