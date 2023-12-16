aa45835f2830b1040568f32060e4b5ecb2e003f6
Ben Lubas <56943754+benlubas@users.noreply.github.com>
2023-12-16 14:33:26 -0500

    fix: window positioning logic (again) (#89)

--- lua/output_window.lua
+++ lua/output_window.lua
@@ -7,10 +7,9 @@ local M = {}
 M.calculate_window_position = function(buf_line)
   local win = vim.api.nvim_get_current_win()
   local pos = vim.fn.screenpos(win, buf_line, 0)
-  local topline = vim.fn.line('w0')
-  local win_origin = vim.fn.screenpos(win, topline, 0)
+  local win_off = vim.fn.getwininfo(win)[1].winrow
 
-  return pos.row - (win_origin.row - 1)
+  return pos.row - (win_off - 1)
 end
 
 return M
