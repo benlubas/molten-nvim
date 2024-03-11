4f8c7334c6ef5b249fb6c0a3f17303acbb26379b
Adam K. <arkelly2@medicine.wisc.edu>
2024-03-11 11:04:21 -0500

    updated wezterm.nvim api
    needs typing/documentation and error handling/input validation. Before I do that I need to figure out how to get the Molten-Nvim plugin options into the canvas?

--- lua/load_wezterm_nvim.lua
+++ lua/load_wezterm_nvim.lua
@@ -1,6 +1,5 @@
 -- loads the wezterm.nvim plugin and exposes methods to the python remote plugin
 local ok, wezterm = pcall(require, "wezterm")
-
 if not ok then
   vim.api.nvim_err_writeln("[Molten] `wezterm.nvim` not found")
   return
@@ -13,18 +12,47 @@ wezterm_api.get_current_pane = function()
   return current_pane_id
 end
 
-wezterm_api.split_pane = function(direction, opts)
-  if direction == "horizontal" then
-    wezterm.exec_sync({ "cli", "split-pane" })
-  elseif direction == "vertical" then
-    wezterm.exec_sync({ "cli", "split-pane" })
-  end
+wezterm_api.wezterm_molten_init = function(initial_pane_id)
+  local direction = "bottom"
+  local size = "35"
+
+  direction = "--" .. (direction or "bottom")
+  size = tostring(size or "35")
+
+  wezterm.exec_sync({ "cli", "split-pane", direction, "--percent", size })
+  wezterm.exec_sync({ "cli", "activate-pane", "--pane-id", tostring(initial_pane_id) })
+  local _, image_pane_id = wezterm.exec_sync({ "cli", "get-pane-direction", "Prev" })
+  return image_pane_id
 end
 
-wezterm_api.wezterm_molten_init = function()
-  local direction = "horizontal"
-  local opts = {}
-  wezterm.exec_sync({ "cli", "split-pane" })
+wezterm_api.send_image = function(path, image_pane_id, initial_pane_id)
+  local placeholder = "wezterm imgcat %s \r"
+  local image = string.format(placeholder, path)
+  wezterm.exec_sync({ "cli", "activate-pane", "--pane-id", image_pane_id })
+  wezterm.exec_sync({
+    "cli",
+    "send-text",
+    "--pane-id",
+    image_pane_id,
+    "--no-paste",
+    image,
+  })
+  wezterm.exec_sync({ "cli", "activate-pane", "--pane-id", initial_pane_id })
 end
 
-return { wezterm = wezterm_api }
+wezterm_api.close_image_pane = function(image_pane_id)
+  local _, out, err = wezterm.exec_sync({
+    "cli",
+    "send-text",
+    "--pane-id",
+    image_pane_id,
+    "--no-paste",
+    "wezterm cli kill-pane --pane-id " .. image_pane_id .. "\r",
+  })
+  print(_)
+  print(out)
+  print(err)
+  return _
+end
+
+return { wezterm_api = wezterm_api }
