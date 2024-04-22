51dd4dbec00991e9d297b48a1480cc13fb035545
benlubas <benmlubas@gmail.com>
2024-04-22 08:41:16 -0400

    fix: bounds check call to screenpos

--- lua/output_window.lua
+++ lua/output_window.lua
@@ -6,7 +6,8 @@ local M = {}
 ---@return number
 M.calculate_window_position = function(buf_line)
   local win = vim.api.nvim_get_current_win()
-  local pos = vim.fn.screenpos(win, buf_line, 0)
+  local num_lines = vim.fn.line("$")
+  local pos = vim.fn.screenpos(win, math.min(num_lines, buf_line), 0)
   local win_off = vim.fn.getwininfo(win)[1].winrow
 
   return pos.row - (win_off - 1)
