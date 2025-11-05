24f6e1656264f46b69230bf95702b12f1402e223
Richard Peña B. <138641726+richardhapb@users.noreply.github.com>
2025-11-05 20:51:32 -0300

    fix: handle missing parse and region cases (#340)

--- lua/remove_comments.lua
+++ lua/remove_comments.lua
@@ -8,6 +8,10 @@ local M = {}
 M.remove_comments = function(str, lang)
   local parser = vim.treesitter.get_string_parser(str, lang)
   local tree = parser:parse()
+  if not tree then
+    -- Return the same string if the parse cannot be found
+    return str
+  end
   local root = tree[1]:root()
   -- create comment query
   local query = vim.treesitter.query.parse(lang, [[((comment) @c (#offset! @c 0 0 0 -1))]])
@@ -16,10 +20,12 @@ M.remove_comments = function(str, lang)
   -- iterate over query match metadata
   for _, _, metadata in query:iter_matches(root, str, root:start(), root:end_(), {}) do
     local region = metadata[1].range
-    local line = region[1] + 1
-    local col_start = region[2]
-    -- remove comment by extracting the text before
-    lines[line] = string.sub(lines[line], 1, col_start)
+    if region then
+      local line = region[1] + 1
+      local col_start = region[2]
+      -- remove comment by extracting the text before
+      lines[line] = string.sub(lines[line], 1, col_start)
+    end
   end
   -- remove blank lines
   lines = vim.tbl_filter(function(line) return line ~= '' end, lines)
